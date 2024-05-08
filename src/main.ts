15398d2854c8060dfdfbead053d6571ba54c6a65
Igor <Bala6@LaptopvonLeyla.fritz.box>
2024-05-08 12:34:37 +0200

    firebase webconnection to the project

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,26 @@ import 'primeflex/primeflex.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { initializeApp } from 'firebase/app'
 
 import PrimeVue from 'primevue/config'
 import Menubar from 'primevue/menubar'
 
 import App from './App.vue'
 import router from './router'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC89YnVjidBbd4l5WQ6iqY02SBnflUp5zc',
+  authDomain: 'interviews-f0b40.firebaseapp.com',
+  projectId: 'interviews-f0b40',
+  storageBucket: 'interviews-f0b40.appspot.com',
+  messagingSenderId: '670020396138',
+  appId: '1:670020396138:web:e185a8615b5f6070959613'
+}
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
 
 const app = createApp(App)
 
